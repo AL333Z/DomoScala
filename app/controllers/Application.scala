1b8909331e5cefa65a968057206d68773b88729c
mattibal <mattibal@gmail.com>
2014-07-07 17:36:51 +0200

    Initial actors and messages draft.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,23 @@
 package controllers
 
+import actors.DeviceActor
+import actors.DeviceActor.On
+import akka.actor.{PoisonPill, Props}
 import play.api._
+import play.api.libs.concurrent.Akka
 import play.api.mvc._
+import play.api.Play.current
 
 object Application extends Controller {
 
   def index = Action {
+
+    val myActor = Akka.system.actorOf(Props[DeviceActor], name = "mydevice")
+    myActor ! On
+    myActor ! PoisonPill
+
     Ok(views.html.index("Your new application is ready."))
+
   }
 
 }
