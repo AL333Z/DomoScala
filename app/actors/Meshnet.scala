aabdbe90f8d7169c79cb7d4bd3eed1a5020fd2ec
mattibal <mattibal@gmail.com>
2014-08-21 17:19:50 +0200

    Completed interconnection between the MeshnetBase Akka actor and the legacy Meshnet Java library.

--- app/actors/Meshnet.scala
+++ app/actors/Meshnet.scala
@@ -1,6 +1,8 @@
 package actors
 
-import actors.MeshnetBase.{SubscribeToMessagesFromDevice, ToDeviceMessage}
+import java.nio.ByteBuffer
+
+import actors.MeshnetBase.{DeviceNotConnected, FromDeviceMessage, SubscribeToMessagesFromDevice, ToDeviceMessage}
 import akka.actor.{ActorRef, Actor, ActorLogging, Props}
 import com.mattibal.meshnet.{ Device, SerialRXTXComm, Layer3Base }
 import gnu.io._
@@ -10,11 +12,11 @@ import scala.util.Try
 import scala.util.Success
 import scala.util.Failure
 
-/**
- * Companion object of MeshnetBase
- */
+
 object MeshnetBase {
+
   def props(port: CommPortIdentifier, name: String): Props = Props(classOf[MeshnetBase], name)
+
   def getGoodPort: Option[CommPortIdentifier] = {
 
     val tryPorts = try {
@@ -43,6 +45,20 @@ object MeshnetBase {
     }
   }
 
+
+  /**
+   * A "scalification" of the legacy Java method...
+   *
+   * Use this instead of Device.getDeviceFromUniqueId()
+   */
+  def getDeviceFromUniqueId(deviceId: Int) : Option[Device] = {
+    Device.getDeviceFromUniqueId(deviceId) match {
+      case null => None
+      case device: Device => Some(device)
+    }
+  }
+
+
   // A message that can be sent to a MeshNet device. All real messages should extend from this.
   case class ToDeviceMessage(destinationId: Int, command: Int, data: Array[Byte])
 
@@ -53,6 +69,12 @@ object MeshnetBase {
    * the device specified as argument
    */
   case class SubscribeToMessagesFromDevice(deviceId: Int)
+
+  /**
+   * This is a notification that the requested operation (message send or receive subscription) has not been executed
+   * because the Meshnet device identified by this deviceId is not currently connected to that MeshnetBase
+   */
+  case class DeviceNotConnected(deviceId: Int)
 }
 
 
@@ -66,7 +88,7 @@ object MeshnetBase {
  * If you want to send or receive messages with devices that are currently
  * connected to a certain MeshNet base, you have to talk with this actor.
  */
-class MeshnetBase(port: CommPortIdentifier) extends Actor with ActorLogging {
+class MeshnetBase(port: CommPortIdentifier) extends Actor with ActorLogging with Device.CommandReceivedListener {
 
   val layer3Base = new Layer3Base
 
@@ -79,19 +101,40 @@ class MeshnetBase(port: CommPortIdentifier) extends Actor with ActorLogging {
 
     // this is blocking, but everything on this actor will be blocking... it just wastes 1 thread for each MeshNet base
     Thread.sleep(4000)
+
+
   }
 
 
   def receive = {
 
     case ToDeviceMessage(destinationId, command, data) => {
-      val device = Device.getDeviceFromUniqueId(destinationId)
-      device.sendCommand(command, data)
+      MeshnetBase.getDeviceFromUniqueId(destinationId) match {
+        case Some(device) => device.sendCommand(command, data)
+        case None => sender() ! DeviceNotConnected(destinationId)
+      }
     }
 
     case SubscribeToMessagesFromDevice(deviceId) => {
-      subscribedActors.put(deviceId, subscribedActors.getOrElse(deviceId, Set())+context.sender())
+      MeshnetBase.getDeviceFromUniqueId(deviceId) match {
+        case Some(device) => {
+          device.addCommandReceivedListener(this)
+          subscribedActors += ((deviceId, subscribedActors.getOrElse(deviceId, Set()) + context.sender()))
+        }
+        case None => sender() ! DeviceNotConnected(deviceId)
+      }
     }
+
+    case msg : FromDeviceMessage => {
+      val subscribers = subscribedActors.get(msg.sourceDevId)
+      subscribers.foreach(_.foreach(_ ! msg))
+    }
+  }
+
+
+  override def onCommandReceived(command: Int, deviceId: Int, data: ByteBuffer): Unit = {
+    // This is called from a Meshnet Java library thread, so I just send myself a message to avoid threading issues
+    context.self ! FromDeviceMessage(deviceId, command, data.array())
   }
 }
 
