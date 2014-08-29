509c17881410fc25cd707bf0dac57bd49366c417
mattibal <mattibal@gmail.com>
2014-08-30 01:08:49 +0200

    Implemented BulbActor physically blink a light

--- app/actors/device/BulbActor.scala
+++ app/actors/device/BulbActor.scala
@@ -5,6 +5,8 @@ import actors.MeshnetBase.ToDeviceMessage
 import akka.actor.{ActorRef, Props, actorRef2Scala}
 import actors.DeviceActor
 import akka.event.LoggingReceive
+import play.libs.Akka
+import scala.concurrent.duration._
 
 object BulbActor {
   def props(name: String, meshnetActor: ActorRef, deviceId: Int): Props = Props(classOf[BulbActor], name, meshnetActor, deviceId)
@@ -13,43 +15,46 @@ object BulbActor {
 class BulbActor(name: String, meshnetActor: ActorRef, deviceId: Int) extends DeviceActor(name, meshnetActor, deviceId) {
 
   var isLampOn = false
+  var intensityValue = 188 // this is a 8 bit PWM value, ranging from 0 to 255
+
+
+  override def preStart() = {
+    // test code to blink lights :)
+    Akka.system.scheduler.schedule(1.6 seconds, 2 seconds, self, On)
+    Akka.system.scheduler.schedule(2.6 seconds, 2 seconds, self, Off)
+  }
+
 
   def receive = LoggingReceive {
     case On => {
-      setLampState(true)
+      isLampOn = true
+      setPwmValue(intensityValue)
       sender ! Ok
     }
     case Off => {
-      setLampState(false)
+      isLampOn = false
+      setPwmValue(0) // switch off lamp
       sender ! Ok
     }
     case SetActivationValue(value) => value match {
       case value if (value < 0.0 || value > 1.0) =>
         sender ! Failed(new Exception("Value out of valid range [0.0 ... 1.0]!"))
-      case value if (value < 0.5) =>
-        setLampState(false)
-        sender ! Ok
-      case value if(value >= 0.5) =>
-        setLampState(true)
+      case value =>
+        intensityValue = (value * 255).toInt
+        isLampOn = true
+        setPwmValue(intensityValue)
         sender ! Ok
     }
     case _ => sender ! UnsupportedAction
   }
 
-  def setLampState(lampOn: Boolean){
-    meshnetActor ! lampSwitchMessage(lampOn)
-    isLampOn = lampOn
-  }
+
+  val meshnetCommand = 2
 
   /**
    * This makes a low-level Meshnet message to be sent to the device
    */
-  def lampSwitchMessage(lampOn: Boolean) = {
-    ToDeviceMessage(deviceId, 2, Array[Byte](
-      lampOn match {
-        case true => 60
-        case false => 0
-      }
-    ))
+  def setPwmValue(pwmValue: Int) = {
+    meshnetActor ! ToDeviceMessage(deviceId, meshnetCommand, Array[Byte]((pwmValue-128).toByte))
   }
 }
