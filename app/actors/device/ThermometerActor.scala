49321108b978a4e9b42bcc890a5db0ad2097bc27
mattibal <mattibal@gmail.com>
2014-08-29 04:13:39 +0200

    Implemented code to read temperature from the real device

--- app/actors/device/ThermometerActor.scala
+++ app/actors/device/ThermometerActor.scala
@@ -1,6 +1,9 @@
 package actors.device
 
+import java.nio.{ByteOrder, ByteBuffer}
+
 import actors.DeviceActor._
+import actors.MeshnetBase.{FromDeviceMessage, SubscribeToMessagesFromDevice, ToDeviceMessage}
 import akka.actor.{ActorRef, Props, actorRef2Scala}
 import actors.DeviceActor
 import akka.event.LoggingReceive
@@ -8,7 +11,6 @@ import play.api.Play.current
 import play.api.libs.concurrent.Akka
 import scala.concurrent.duration._
 import play.api.libs.concurrent.Execution.Implicits._
-import scala.util.Random
 
 
 object ThermometerActor {
@@ -17,13 +19,54 @@ object ThermometerActor {
 
 class ThermometerActor(name: String, meshnetActor: ActorRef, deviceId: Int) extends DeviceActor(name, meshnetActor, deviceId) {
 
-  Akka.system.scheduler.schedule(1000 milliseconds, 2000 milliseconds) {
-    println("Gonna pubblish new temperature value..")
-    Akka.system.eventStream.publish(TemperatureValue(Random.nextFloat, Some(self.path.name)))
+  /**
+   * This is a sort of cached temperature value
+   */
+  var currTemp: Option[Double] = None
+
+
+  override def preStart() = {
+    meshnetActor ! SubscribeToMessagesFromDevice(deviceId)
+    Akka.system.scheduler.schedule(2 seconds, 3 seconds, self, AskTemp) // periodically ask temperature to the device
   }
 
+  case object AskTemp
+
   def receive = LoggingReceive {
-    case GetTemperature => sender ! TemperatureValue(30.0f)
+    case GetTemperature => currTemp.foreach(sender ! TemperatureValue(_))
+    case AskTemp => askTemperatureToDevice()
+    case FromDeviceMessage => parseFromDeviceMessage(_)
     case _ => sender ! UnsupportedAction
+  }
+
+
+  /**
+   * Send a Meshnet message to the device asking a temperature reading from the temperature sensor
+   */
+  def askTemperatureToDevice(){
+    meshnetActor ! ToDeviceMessage(deviceId, 3, Array[Byte](0))
+  }
+
+  /**
+   * Handle a message coming from the Meshnet device that hopefully contains a temperature value
+   */
+  def parseFromDeviceMessage(msg: FromDeviceMessage){
+    if(msg.command == 3){
+
+      // very low level scary stuff!!
+      val buf = ByteBuffer.wrap(msg.data)
+      buf.order(ByteOrder.LITTLE_ENDIAN)
+      buf.position(3)
+      val analogRead = buf.getShort.toInt & 0xffff // this is the ADC reading from the thermistor
+      val pad = 11000d; // balance/pad resistor resistance value
+      val thermistorResistance = (1024 * pad / analogRead.toDouble) - pad
+      val logRes = Math.log(thermistorResistance)
+      val tempKelvin = 1 / (0.001129148 + (0.000234125 * logRes) + (0.0000000876741 * math.pow(logRes,3))) // magical numbers, yay!!
+      val tempCelsius = tempKelvin - 273.15
+
+      // back to Scala...
+      currTemp = Some(tempCelsius)
+      Akka.system.eventStream.publish(TemperatureValue(tempCelsius))
+    }
   }
 }
