abeaa8438ccdfce2907dcd4413dccefe2ebd2312
mattibal <mattibal@gmail.com>
2014-09-02 01:04:17 +0200

    Implemented button, light sensor and thermometer mock actors

---
+++ app/actors/device/mock/ThermometerMockActor.scala
@@ -0,0 +1,29 @@
+package actors.device.mock
+
+import actors.DeviceActor.{UnsupportedAction, GetTemperature, TemperatureValue}
+import akka.actor.Actor
+import play.api.libs.concurrent.Akka
+import scala.concurrent.duration._
+import scala.util.Random
+import play.api.libs.concurrent.Execution.Implicits._
+
+
+class ThermometerMockActor extends Actor{
+
+  override def preStart = {
+    Akka.system.scheduler.schedule(2 seconds, 3 seconds, self, SendTemp) // periodically send fake temperature reading
+  }
+
+  case object SendTemp
+
+  def receive = {
+    case SendTemp =>
+      Akka.system.eventStream.publish(TemperatureValue(fakeTemp, Some(self.path.name)))
+    case GetTemperature =>
+      sender ! TemperatureValue(fakeTemp)
+    case _ => sender ! UnsupportedAction
+  }
+
+  def fakeTemp = 10 + (Random.nextDouble()*20)
+
+}
