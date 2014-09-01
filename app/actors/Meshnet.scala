27752a7adc64950be1fa4eff4c1a991e60db0a87
mattibal <mattibal@gmail.com>
2014-09-02 00:41:14 +0200

    Modified device names to standard names with a progressive number (ex. "Bulb0", "Bulb1"...)

--- app/actors/Meshnet.scala
+++ app/actors/Meshnet.scala
@@ -153,21 +153,21 @@ class MeshnetBase(port: CommPortIdentifier, domoscalaActor: ActorRef) extends Ac
       _ match {
         case device: Led1Analog2Device => {   // this is our battery-powered wireless test circuit
           val devId = device.getUniqueId
-          val bulb = context.actorOf(BulbActor.props("Bulb"+devId, self, devId), "Bulb"+devId)
-          val temp = context.actorOf(ThermometerActor.props("Thermometer"+devId, self, devId), "Thermometer"+devId)
-          val light = context.actorOf(LightSensorActor.props("LightSensor"+devId, self, devId), "LightSensor"+devId)
-          Room("Room1", Map("Bulb"+devId -> bulb, "Thermometer"+devId -> temp, "LightSensor"+devId -> light))
+          val bulb = context.actorOf(BulbActor.props("Bulb1", self, devId), "Bulb1")
+          val temp = context.actorOf(ThermometerActor.props("Thermometer0", self, devId), "Thermometer0")
+          val light = context.actorOf(LightSensorActor.props("LightSensor0", self, devId), "LightSensor0")
+          Room("Room1", Map("Bulb1" -> bulb, "Thermometer0" -> temp, "LightSensor0" -> light))
         }
 
         case device: Device => {       // this is our Arduino shield connected with USB to the computer (Meshnet base)
           val devId = device.getUniqueId
-          val bulb = context.actorOf(BulbActor.props("Bulb"+devId, self, devId), "Bulb"+devId)
-          val button = context.actorOf(ButtonActor.props("Button"+devId, self, devId), "Button"+devId)
-          Room("Room2", Map("Bulb"+devId -> bulb, "Button"+devId -> button))
+          val bulb = context.actorOf(BulbActor.props("Bulb0", self, devId), "Bulb")
+          val button = context.actorOf(ButtonActor.props("Button0", self, devId), "Button0")
+          Room("Room0", Map("Bulb0" -> bulb, "Button0" -> button))
         }
       }
     )
-    val building = Building("Building1", Set()++rooms) // ++ needed to convert from mutable to immutable Set
+    val building = Building("Building0", Set()++rooms) // ++ needed to convert from mutable to immutable Set
     domoscalaActor ! AddBuilding(building)
   }
 }
